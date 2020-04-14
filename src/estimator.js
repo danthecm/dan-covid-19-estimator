7ee85d43d2077b05cfe8523467a6ac0df0bbfeee
CM <etchiedaniel1@gmail.com>
2020-04-14 16:55:32 +0100

    hu

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,23 @@
+const personal = (data) => {
+  let calulating;
+  if(data.periodType === "days") {
+    calulating = 2 ** ((data.timeToElapse) / 3);
+  }
+
 const covid19ImpactEstimator = (data) => {
   const input = data;
   return {
     data: input,
     impact: {
       currentlyInfected: data.reportedCases * 10,
-      infectionsByRequestedTime: Math.trunc((data.reportedCases) * (10) * 512)
+      infectionsByRequestedTime: Math.trunc((data.reportedCases) * (10) * calulating)
     },
     severeImpact: {
       currentlyInfected: data.reportedCases * 50,
-      infectionsByRequestedTime: Math.trunc((data.reportedCases) * (50) * 512)
+      infectionsByRequestedTime: Math.trunc((data.reportedCases) * (50) * calulating)
     }
   };
 };
-export default covid19ImpactEstimator;
+  export default covid19ImpactEstimator;
+};
+
