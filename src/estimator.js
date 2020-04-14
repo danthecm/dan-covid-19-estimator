54c573461cb47635409321cbb123ffaf314ad375
CM <etchiedaniel1@gmail.com>
2020-04-14 15:41:18 +0100

    AS

--- src/estimator.js
+++ src/estimator.js
@@ -2,21 +2,21 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const calulating = () => {
     if (input.periodType === 'days')
-    return (2 ** (input.timeToElapse / 3));
+      return (2 ** (input.timeToElapse / 3));
     if (input.periodType === 'weeks')
-    return (2 ** ((input.timeToElapse / 7) / 3));
+      return (2 ** ((input.timeToElapse / 7) / 3));
     if (input.periodType === 'months')
-    return (2 ** ((input.timeToElapse / 30) / 3));
+      return (2 ** ((input.timeToElapse / 30) / 3));
   };
   return {
     data: input,
     impact: {
-        currentlyInfected: data.reportedCases * 10,
-        infectionsByRequestedTime: Math.trunc((data.reportedCases) * (10) * (calulating()))
+      currentlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: Math.trunc((data.reportedCases) * (10) * (calulating()))
     },
     severeImpact: {
-        currentlyInfected: data.reportedCases * 50,
-        infectionsByRequestedTime: Math.trunc((data.reportedCases) * (50) * (calulating()))
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: Math.trunc((data.reportedCases) * (50) * (calulating()))
     }
   };
 };
